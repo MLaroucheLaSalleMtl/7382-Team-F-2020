e8a2b83c1cbb717b308691a10875f1aa400572ef
Boren <56322187+jopjiang@users.noreply.github.com>
2020-03-30 21:35:25 -0400

    New Skills
    Seperated script skill

---
+++ Assets/G.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class G : MonoBehaviour
+{
+    private Image iconG;
+    private bool isGSkill;
+    private bool isGLock;
+    public PlayerMove move;
+    private bool isStartTimerG;
+    private float timerG;
+    public float skillStopTime;
+    public float cooldownTimeG;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        iconG = transform.Find("MaskG").GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("SkillG") && isGSkill == false && isGLock == false)
+        {
+            isGLock = true;
+            isGSkill = true;
+            move.speed = 10;
+            isStartTimerG = true;
+        }
+
+        if (isStartTimerG == true)
+        {
+            timerG += Time.deltaTime;
+            iconG.fillAmount = (cooldownTimeG - timerG) / cooldownTimeG;            
+        }
+        if (timerG >= skillStopTime)
+        {
+            if (isGSkill == true)
+            {
+                move.speed = 4;
+                isGSkill = false;
+            }
+        }
+        if (timerG >= cooldownTimeG)
+        {
+            iconG.fillAmount = 0;
+            timerG = 0;
+            isGLock = false;
+            isStartTimerG = false;
+        }
+    }
+}
